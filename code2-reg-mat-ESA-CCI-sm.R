9f573fea74b744d21ef45abb27e2ab44eb2159f7
marioguevara <mguevara@udel.edu>
2018-07-04 16:57:44 -0400

    Update code2-reg-mat-ESA-CCI-sm.R

--- code2-reg-mat-ESA-CCI-sm.R
+++ code2-reg-mat-ESA-CCI-sm.R
@@ -3,26 +3,33 @@ library(caret)
 library(doMC)
 library(doParallel)
 
-sm <- readRDS('SM-COL-2016.rds')
-topo <- stack(readRDS('/home/mario/Downloads/Colombia/humedad/topoCOL.rds'))
-smrm <- cbind(sm@data, extract(topo, sm))
+sm <- readRDS('SM-VEN-2016.rds')
+
+wg <- stack('/home/mario/Downloads/Colombia/humedad/VEN_worldgridsCOVS.tif')
+names(wg) <- readRDS('/home/mario/Downloads/Colombia/humedad/worldgridsCOVS_names.rds')
+
+smrm <- cbind(sm@data, extract(wg, sm))
+
+saveRDS(smrm, file='reg-mat-sm-cci-2016-VEN.rds')
+
 smrm$averageSM <-rowMeans(smrm[1:366], na.rm = TRUE)
-smX <- na.omit(smrm[367:382])
+smX <- na.omit(smrm[367:485])
 
 ##VARIABLE IMPORTANCE 
 
 cl <- makeCluster(detectCores(), type='PSOCK')
 registerDoParallel(cl)
 control2 <- rfeControl(functions=rfFuncs, method="repeatedcv", number=5, repeats=5)
-(RFE_RF_model <- rfe(smX[-16], smX[,16], sizes=c(1:6), rfeControl=control2) )
+(RFE_RF_model <- rfe(smX[-119], smX[,119], sizes=c(1:6), rfeControl=control2) )
+
 plot(RFE_RF_model, type=c("g", "o"))
-predictors(RFE_RF_model)[1:8]
+predictors(RFE_RF_model)
 stopCluster(cl = cl)
 
 ##SOIL MOISTURE PREDICTION
 
 beginCluster(5)
 library(randomForest)
-(predRFE <- clusterR(topo, predict, args=list(model=RFE_RF_model)))
+(predRFE <- clusterR(wg, predict, args=list(model=RFE_RF_model)))
 plot(predRFE, col=colorRampPalette(c("gray", "brown", "blue"))(255))
 endCluster()
