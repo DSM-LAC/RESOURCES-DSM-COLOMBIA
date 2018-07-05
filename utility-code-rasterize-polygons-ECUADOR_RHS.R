815628096435eb8776e35b149cc87b9bec8173ff
marioguevara <mguevara@udel.edu>
2018-07-05 17:31:32 -0400

    Create utility-code-rasterize-polygons-ECUADOR_RHS.R

---
+++ utility-code-rasterize-polygons-ECUADOR_RHS.R
@@ -0,0 +1,16 @@
+#load libraries
+library(raster)
+library(rgdal)
+#load reference raster or raster stack
+s <- stack('ECUtopo.tif')
+#read polygon to rasterize
+rh <- readOGR(dsn=getwd(), layer="R_HUMEDAD_SUELOS_RHS_ECU")
+#rasterize
+ras <- rasterize(rh, s, "RHS")
+ras <- as.factor(ras)
+levels(ras) <- levels(rh$RHS)
+#save new raster
+writeRaster(f, file='rasterizedRHS.tif')
+#add new raster to topographic predictors
+#s <- stack(s, SPP2)
+#writeRaster(s, file='ECUtopo_RHS.tif')
